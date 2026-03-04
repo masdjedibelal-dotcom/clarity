53514752c8ad895917be87d48ebcdb41b0921170
masdjedibelal-dotcom <masdjedibelal@gmail.com>
2026-03-04 23:38:55 +0100

    Delete lib directory

--- lib/data/supabase/supabase_adapter.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseAdapter {
-  // TODO: Supabase client init
-  // TODO: Implement fetch for knowledge/inner/identity/system
-  // TODO: Persist user state (saved IDs, identity selections, day plan)
-}
